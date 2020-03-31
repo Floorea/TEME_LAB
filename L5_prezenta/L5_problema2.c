014812a9d21c1418b99088c1417e006e8a40c295
Floorea <florea4lex@gmail.com>
2020-03-31 12:57:55 +0300

    added problema 2 (prezenta lab 5)

---
+++ L5_prezenta/L5_problema2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+int stack[MAX_SIZE];
+int top = -1;
+
+void push(int a)
+{
+	if (top == MAX_SIZE - 1)
+		printf("Error: stack overflow\n");
+	else
+		stack[++top] = a;
+}
+
+void print() 
+{
+	if (top == -1)
+		printf("Error: no elements in stack\n");
+	else
+		for (int i = 0; i <= top; i++)
+			printf("%d ", stack[i]);
+	printf("\n");
+
+}
+
+int main()
+{
+	int C, N, i,nr;
+	
+line:printf("C= "); 
+	
+	if (scanf("%d", &C) > 0);
+	if (C > 7 || C < 0)
+	{
+		printf("Error: wrong 'C' value \n");
+		goto line;
+	}
+	printf("N= "); if(scanf("%d", &N)>0);
+
+	for (i = 0; i < N; i++)
+	{
+		if(scanf("%d", &nr)>0);
+		if ((nr & (1 << C)) == (1 << C))
+			push(nr);
+	}
+
+	print();
+
+	return 0;
+
+}
